5afdc7cceb5a755343d8164ae516b901028f65cc
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-09-03 12:29:09 -0400

    Creando componente donde se encuentran los metodos

---
+++ app/src/main/java/edu/ucne/registro_prioridades/local/dao/PrioridadDao.kt
@@ -0,0 +1,31 @@
+package edu.ucne.registro_prioridades.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registro_prioridades.local.database.PrioridadDb
+import edu.ucne.registro_prioridades.local.entities.PrioridadEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert
+    suspend fun save(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM Prioridades WHERE prioridadId = :id LIMIT 1")
+    suspend fun find(id: Int): PrioridadEntity?
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM Prioridades")
+    fun getAll(): Flow<List<PrioridadEntity>>
+
+
+    @Query("SELECT * FROM Prioridades WHERE descripcion = :descripcion LIMIT 1")
+    suspend fun findByDescripcion(descripcion: String): PrioridadEntity?
+}
+
+
+
