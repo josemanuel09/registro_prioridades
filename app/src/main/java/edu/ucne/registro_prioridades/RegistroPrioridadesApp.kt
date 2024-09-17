862faf32121ab9aee31366b33d495ea1c99f2a7f
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-09-17 19:57:53 -0400

    Creando clase que hereda de Aplication

---
+++ app/src/main/java/edu/ucne/registro_prioridades/RegistroPrioridadesApp.kt
@@ -0,0 +1,8 @@
+package edu.ucne.registro_prioridades
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroPrioridadesApp: Application() {
+}
