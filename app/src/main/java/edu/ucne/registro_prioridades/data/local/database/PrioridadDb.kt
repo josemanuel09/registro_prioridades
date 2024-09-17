7bef6606917adb2e79fa364c42c8d684080440fb
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-09-17 19:54:28 -0400

    Modificando PrioridadDb

---
+++ app/src/main/java/edu/ucne/registro_prioridades/data/local/database/PrioridadDb.kt
@@ -0,0 +1,24 @@
+package  edu.ucne.registro_prioridades.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import  edu.ucne.registro_prioridades.data.local.dao.PrioridadDao
+import edu.ucne.registro_prioridades.data.local.dao.TicketDao
+import edu.ucne.registro_prioridades.data.local.entities.PrioridadEntity
+import edu.ucne.registro_prioridades.data.local.entities.TicketEntity
+
+
+@Database(
+    entities = [
+        PrioridadEntity::class,
+        TicketEntity::class
+    ],
+
+    version = 2,
+    exportSchema = false
+)
+abstract class PrioridadDb : RoomDatabase() {
+    abstract fun prioridadDao() : PrioridadDao
+    abstract fun ticketDao() : TicketDao
+
+}
