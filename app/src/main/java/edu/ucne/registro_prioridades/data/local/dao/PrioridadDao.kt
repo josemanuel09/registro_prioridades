34d1355236bd37b9537e72d499e139fc8dbeece4
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-09-17 19:54:01 -0400

    Modificando PrioridadDao

--- app/src/main/java/edu/ucne/registro_prioridades/data/local/dao/PrioridadDao.kt
+++ app/src/main/java/edu/ucne/registro_prioridades/data/local/dao/PrioridadDao.kt
@@ -1,11 +1,11 @@
-package edu.ucne.registro_prioridades.local.dao
+package edu.ucne.registro_prioridades.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
-import edu.ucne.registro_prioridades.local.database.PrioridadDb
-import edu.ucne.registro_prioridades.local.entities.PrioridadEntity
+import edu.ucne.registro_prioridades.data.local.database.PrioridadDb
+import edu.ucne.registro_prioridades.data.local.entities.PrioridadEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
