c2bef9777e22000baa32cb735ce86c842a5d8219
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-10-09 17:10:58 -0400

    Creando el SistemasListScreen

---
+++ app/src/main/java/edu/ucne/registro_prioridades/presentation/sistemas/SistemasListScreen.kt
@@ -0,0 +1,190 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package edu.ucne.registro_prioridades.presentation.sistemas
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registro_prioridades.data.remote.dto.SistemasDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun SistemaListScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    viewModel: SistemasViewModel = hiltViewModel(),
+    createSistema: () -> Unit,
+    onEditSistema: (Int) -> Unit,
+    onDeleteSistema: (Int) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    SistemaListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState = uiState,
+        createSistema = createSistema,
+        onEditSistema = onEditSistema,
+        onDeleteSistema = onDeleteSistema
+    )
+}
+
+@Composable
+fun SistemaListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: SistemasViewModel.Uistate,
+    createSistema: () -> Unit,
+    onEditSistema: (Int) -> Unit,
+    onDeleteSistema: (Int) -> Unit
+) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Sistemas",
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        scope.launch { drawerState.open() }
+                    }) {
+                        Icon(imageVector = Icons.Default.Menu, contentDescription = "Ir al menú")
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(0xFF6200EE)
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = createSistema,
+                modifier = Modifier.padding(16.dp),
+                containerColor = MaterialTheme.colorScheme.primary,
+                contentColor = Color.White
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Añadir Sistema")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(uiState.sistemas) { sistema ->
+                    SistemaRow(sistema, onEditSistema, onDeleteSistema)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SistemaRow(
+    sistema: SistemasDto,
+    onEditSistema: (Int) -> Unit,
+    onDeleteSistema: (Int) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(5f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+
+                    text = "NombreSistema: ${sistema.NombreSistema}",
+                    style = TextStyle(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+                    Text(
+                        text = "ID: ${sistema.sistemaId}",
+                        style = TextStyle(
+                            fontSize = 14.sp,
+                            color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                        )
+                    )
+                }
+            }
+
+            IconButton(
+                onClick = { expanded = !expanded },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = "Más opciones")
+            }
+
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+                modifier = Modifier.background(MaterialTheme.colorScheme.surface)
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Editar") },
+                    leadingIcon = { Icon(Icons.Filled.Edit, contentDescription = "Editar") },
+                    onClick = {
+                        expanded = false
+                        onEditSistema(sistema.sistemaId ?: 0)
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text("Eliminar") },
+                    leadingIcon = { Icon(Icons.Filled.Delete, contentDescription = "Eliminar") },
+                    onClick = {
+                        expanded = false
+                        onDeleteSistema(sistema.sistemaId ?: 0)
+                    }
+                )
+            }
+        }
+    }
+
